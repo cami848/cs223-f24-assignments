7a3ee198a1b15e800ba67426e43bec4f346a5a63
Camille Lopez <cmlllopez@gmail.com>
2024-12-05 19:55:28 -0500

    Assignment #12 Complete

--- A12/memstats.c
+++ A12/memstats.c
@@ -1,10 +1,18 @@
-#include <stdlib.h>
+/*----------------------------------------------
+ * Author: Camille Lopez
+ * Date:12/5/2024
+ * Description: This program implements a
+ * function memstats to print information
+ * about the heap.
+ ---------------------------------------------*/
+
+#include "rand.h"
 #include <stdio.h>
-#include <unistd.h>
+#include <stdlib.h>
+#include <string.h>
 #include <sys/time.h>
 #include <time.h>
-#include <string.h>
-#include "rand.h"
+#include <unistd.h>
 
 #define ROUNDS 3
 #define BUFFER 5
@@ -16,10 +24,54 @@ struct chunk {
   struct chunk *next;
 };
 
-void memstats(struct chunk* freelist, void* buffer[], int len) {
+/**
+ * Function that gets the memory statistics
+ * @param freelist  Pointer to thread_data
+ * @param buffer  Pointer to an array
+ * @param len length of buffer array
+ * @return void
+ */
+void memstats(struct chunk *freelist, void *buffer[], int len) {
+  int total_blocks = 0;
+  int free_blocks = 0;
+  int used_blocks = 0;
+  int memory_allocated = 0;
+  int free_memory = 0;
+  int used_memory = 0;
+  int utilized_memory = 0;
+  float underutilized_memory = 0;
+
+  for (int i = 0; i < len; i++) {
+    struct chunk *current = (struct chunk *)buffer[i] - 1;
+    if (buffer[i] != NULL) {
+      total_blocks++;
+      used_blocks++;
+      used_memory += current->size;
+      utilized_memory += current->used;
+      memory_allocated += current->size;
+    }
+  }
+  struct chunk *current_node = freelist;
+  underutilized_memory = 1 - (float)(utilized_memory) / (float)(used_memory);
+  while (current_node) {
+    free_blocks++;
+    total_blocks++;
+    free_memory += current_node->size;
+    memory_allocated += current_node->size;
+    current_node = current_node->next;
+  }
+
+  // Print the stats
+  printf("Total blocks: %d ", total_blocks);
+  printf(" Free blocks: %d ", free_blocks);
+  printf(" Used blocks: %d \n", used_blocks);
+  printf("Total memory allocated: %d ", memory_allocated);
+  printf(" Free memory: %d ", free_memory);
+  printf(" Used memory: %d \n", used_memory);
+  printf("Underutilized memory: %.2f \n", underutilized_memory);
 }
 
-int main ( int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
 
   printf("Starting test..\n");
 
@@ -38,31 +90,30 @@ int main ( int argc, char* argv[]) {
   void *init = sbrk(0);
   void *current;
   printf("The initial top of the heap is %p.\n", init);
-  for (int j = 0 ; j < ROUNDS; j++) {
-    printf("---------------\n%d\n" , j);
+  for (int j = 0; j < ROUNDS; j++) {
+    printf("---------------\n%d\n", j);
 
-    for (int i= 0 ; i < LOOP ; i++) {
+    for (int i = 0; i < LOOP; i++) {
       int index = rand() % BUFFER;
       if (buffer[index] != NULL) {
         free(buffer[index]);
         buffer[index] = NULL;
         printf("Freeing index %d\n", index);
-      } 
-      else {
-        size_t size = (size_t) randExp(8, 4000); 
+      } else {
+        size_t size = (size_t)randExp(8, 4000);
         int *memory = NULL;
         memory = malloc(size);
 
         if (memory == NULL) {
           fprintf(stderr, "malloc failed\n");
-          return(1);
-        } 
+          return (1);
+        }
         *memory = 123;
         buffer[index] = memory;
-        printf("Allocating %d bytes at index %d\n", (int) size, index);
+        printf("Allocating %d bytes at index %d\n", (int)size, index);
       }
     }
-    extern struct chunk* flist;
+    extern struct chunk *flist;
     current = sbrk(0);
     int allocated = current - init;
     init = current;
@@ -73,11 +124,11 @@ int main ( int argc, char* argv[]) {
   }
 
   for (int i = 0; i < BUFFER; i++) {
-    free(buffer[i]); 
+    free(buffer[i]);
   }
   gettimeofday(&tend, NULL);
-  timer = tend.tv_sec - tstart.tv_sec + (tend.tv_usec - tstart.tv_usec)/1.e6;
+  timer = tend.tv_sec - tstart.tv_sec + (tend.tv_usec - tstart.tv_usec) / 1.e6;
   printf("Time is %g\n", timer);
 
-  return 0 ;
+  return 0;
 }
--- A12/mylloc_list.c
+++ A12/mylloc_list.c
@@ -1,3 +1,11 @@
+/*----------------------------------------------
+ * Author: Camille Lopez
+ * Date:12/5/2024
+ * Description: This program implements our
+ * own version of malloc and free based on the
+ * assigned reading.
+ ---------------------------------------------*/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -9,12 +17,55 @@ struct chunk {
 };
 struct chunk *flist = NULL;
 
-void *malloc (size_t size) {
+/**
+ * Function that mallocs memory
+ * @param size  size of memory
+ * @return void
+ */
+void *malloc(size_t size) {
   // TODO: Implement malloc with a free list (See reading for details)
-  return NULL;
+  if (size == 0) {
+    return NULL;
+  }
+  struct chunk *next = flist;
+  struct chunk *prev = NULL;
+  while (next != NULL) {
+    if (next->size >= size) {
+      next->used = size;
+      if (prev != NULL) {
+        prev->next = next->next;
+      } else {
+        flist = next->next;
+      }
+      return (void *)(next + 1);
+    } else {
+      prev = next;
+      next = next->next;
+    }
+  }
+  void *memory = sbrk(size + sizeof(struct chunk));
+  if (memory == (void *)-1) {
+    return NULL;
+  } else {
+    struct chunk *cnk = (struct chunk *)memory;
+    cnk->size = size;
+    cnk->used = size;
+    return (void *)(cnk + 1);
+  }
 }
 
+/**
+ * Function that puts freed memory in a freelist
+ * @param memory  Pointer to memory we are freeing
+ * @return void
+ */
 void free(void *memory) {
   // TODO: Implement malloc with a free list (See reading for details)
+  if (memory != NULL) {
+    struct chunk *cnk = (struct chunk *)((struct chunk *)memory - 1);
+    cnk->next = flist;
+    flist = cnk;
+    flist->used = 0;
+  }
   return;
 }
